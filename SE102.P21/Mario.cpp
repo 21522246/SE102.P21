2cd5896c851eee8a7f081716ee4b94c3708a004f
21522246 <21522246@gm.uit.edu.vn>
2025-04-22 21:11:02 +0700

    feat: add Mushroom and change Mario inital state to be Small

--- SE102.P21/Mario.cpp
+++ SE102.P21/Mario.cpp
@@ -7,6 +7,7 @@
 #include "Goomba.h"
 #include "Coin.h"
 #include "Portal.h"
+#include "Mushroom.h"
 
 #include "Collision.h"
 
@@ -53,6 +54,8 @@ void CMario::OnCollisionWith(LPCOLLISIONEVENT e)
 		OnCollisionWithCoin(e);
 	else if (dynamic_cast<CPortal*>(e->obj))
 		OnCollisionWithPortal(e);
+	else if (dynamic_cast<CMushroom*>(e->obj))
+		OnCollisionWithMushroom(e);
 }
 
 void CMario::OnCollisionWithGoomba(LPCOLLISIONEVENT e)
@@ -99,6 +102,18 @@ void CMario::OnCollisionWithPortal(LPCOLLISIONEVENT e)
 {
 	CPortal* p = (CPortal*)e->obj;
 	CGame::GetInstance()->InitiateSwitchScene(p->GetSceneId());
+}
+
+void CMario::OnCollisionWithMushroom(LPCOLLISIONEVENT e)
+{
+	e->obj->Delete();
+	if (level == MARIO_LEVEL_SMALL)
+	{
+		StartUntouchable();
+		y -= 8;
+		level = MARIO_LEVEL_BIG;
+		StartUntouchable();
+	}
 }
 
 //
